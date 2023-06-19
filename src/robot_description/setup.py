971d19d9efc294722386d7f8d69e208d770a98dc
Lance Fowler <lance.fowler@plp.com>
2023-06-19 13:32:40 -0400

    added files

---
+++ src/robot_description/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'robot_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.urdf')) + glob(os.path.join('urdf', '*.png')) + glob(os.path.join('urdf', '*.dae')))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='lance',
+    maintainer_email='lance@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
