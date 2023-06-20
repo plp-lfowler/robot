22306287806bf16fc3d703af8a0d26e31a7c72ff
Lance Fowler <lance.fowler@plp.com>
2023-06-20 09:58:04 -0400

    moveit_config

---
+++ build/moveit_config/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lance/robot/src/moveit_config
+# Build directory: /home/lance/robot/build/moveit_config
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
